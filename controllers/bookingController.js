84a26e14fa8629549e45a1db00a1fdbd66f9095c
jyotisko <jyotisko2006@gmail.com>
2021-06-21 23:06:25 +0530

    Stripe ready for production

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,7 +54,7 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 const createBookingCheckout = catchAsync(async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({
     user: user,
     tour: tour,
