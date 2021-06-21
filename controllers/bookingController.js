66728cce97166ea29a97e7aea07006f7311fb01d
jyotisko <jyotisko2006@gmail.com>
2021-06-21 23:52:07 +0530

    Changed production image source URL

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -21,7 +21,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
